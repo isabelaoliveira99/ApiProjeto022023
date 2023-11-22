51679aa4e863649d475dd4e8282f59984bc38350
Isabela Oliveira <isabelaoliveiracontato@gmail.com>
2023-11-21 23:45:14 -0300

    Endpoint get para retornar lista de todos os produtos

---
+++ ApiProjeto/Controllers/ProdutosController.cs
@@ -0,0 +1,53 @@
+﻿using ApiProjeto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ApiProjeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class ProdutosController : ControllerBase
+    {
+        private readonly string MySqlConnection = @"Server=20.226.185.146;Port=3306;Database=bd_projeto;Uid=root;Pwd=1234;";
+
+        [HttpGet]
+        public IActionResult RetornaProdutos()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(MySqlConnection);
+
+                conn.Open();
+
+                string sql = @"SELECT * FROM PRODUTO";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                var reader = cmd.ExecuteReader();
+
+                List<Produto> lista = new List<Produto>();
+
+                while (reader.Read())
+                {
+                    Produto item = new Produto();
+                    item.id = int.Parse(reader["id"].ToString());
+                    item.nome_produto = reader["nome_produto"].ToString();
+                    item.descricao_produto = reader["descricao_produto"].ToString();
+                    item.valor = decimal.Parse(reader["valor"].ToString());
+                    item.id_categoria = int.Parse(reader["id_categoria"].ToString());
+                    lista.Add(item);
+                }
+
+              
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ApiProjeto/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeto.Models
+{
+    public class Produto
+    {
+        public int? id { get; set; }
+        public string? nome_produto { get; set; }
+        public string? descricao_produto { get; set; }
+        public decimal? valor { get; set; }
+        public int? id_categoria { get; set; }
+    }
+}
